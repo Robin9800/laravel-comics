a0fb39060ded69177266ede5d31d29d0ed3ead05
Robin9800 <robin.costanzo@gmail.com>
2022-05-18 17:58:45 +0200

    footer content added

--- laravel-comics/resources/views/home.blade.php
+++ laravel-comics/resources/views/home.blade.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     @include('partials/header') 
+    @include('partials/footer') 
 </body>
 </html>
---
+++ laravel-comics/resources/views/partials/footer.blade.php
@@ -0,0 +1,106 @@
+<footer>
+      <div class="header-footer">
+        <ul class="info flex">
+          <li>
+              <img src="images/buy-comics-digital-comics.png" alt="">
+              <h4>DIGITAL COMICS</h4>
+          </li>
+          <li>
+              <img src="images/buy-comics-merchandise.png" alt="">
+              <h4>DC MERCHANDISE</h4>
+          </li>
+          <li>
+              <img src="images/buy-comics-subscriptions.png" alt="">
+              <h4>SCUBSCRIPTION</h4>
+          </li>
+          <li>
+              <img src="images/buy-comics-shop-locator.png" alt="">
+              <h4>COMIC SHOP LOCATOR</h4>
+          </li>
+          <li>
+              <img src="images/buy-dc-power-visa.svg" alt="">
+              <h4>DC POWER VISA</h4>
+          </li>
+        </ul>
+      </div>
+
+      <div class="main-footer flex">
+          <div class="indirizzi">
+              <ul>
+                <h4>DC COMICS</h4>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">Tv</a></li>
+                <li><a href="#">Game</a></li>
+                <li><a href="#">Collectibles</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">Fans</a></li>
+                <li><a href="#">New</a></li>
+                <li><a href="#">Shop</a></li>
+          </ul>
+          <ul>
+                <h4>DC</h4>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">Tv</a></li>
+                <li><a href="#">Game</a></li>
+                <li><a href="#">Collectibles</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">Fans</a></li>
+                <li><a href="#">New</a></li>
+                <li><a href="#">Shop</a></li>
+          </ul>
+          <ul>
+                <h4>SITES</h4>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">Tv</a></li>
+                <li><a href="#">Game</a></li>
+                <li><a href="#">Collectibles</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">Fans</a></li>
+                <li><a href="#">New</a></li>
+                <li><a href="#">Shop</a></li>
+          </ul>
+          <ul>
+                <h4>SHOP</h4>
+                <li><a href="#">Shop DC</a></li>
+                <li><a href="#">Shop DC Collectibles</a></li>
+               
+          </ul>
+          </div>
+          <div class="logo-dc">
+              <img src="images/dc-logo.png" alt="">
+          </div>
+      </div>
+      <div class="footer-container flex">
+
+          <div class="footer-footer flex">
+            <button class="sign-up">SIGN-UP NOW!</button>
+          </div>
+          <div class="social flex">
+              <a class="follow" href="#">FOLLOW US</a>
+              <ul>
+                  <li>
+                      <img src="images/footer-facebook.png" alt="">
+                  </li>
+                  <li>
+                      <img src="images/footer-periscope.png" alt="">
+                  </li>
+                  <li>
+                      <img src="images/footer-pinterest.png" alt="">
+                  </li>
+                  <li>
+                      <img src="images/footer-twitter.png" alt="">
+                  </li>
+                  <li>
+                      <img src="images/footer-youtube.png" alt="">
+                  </li>
+              </ul>
+          </div>
+        </div>
+      
+  </footer>
