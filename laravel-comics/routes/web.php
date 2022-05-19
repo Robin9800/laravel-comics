c2db11f944634d9f3755bdd66e48806139ab192c
Robin9800 <robin.costanzo@gmail.com>
2022-05-19 10:22:02 +0200

    $comics added in "@section('content)"

--- laravel-comics/routes/web.php
+++ laravel-comics/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+    //Usiamo 'config' per collegarci direttamente alla cartella omonima
+    $data = config('comics');
+
+    return view('home', ["comics" => $data ]);
 });
