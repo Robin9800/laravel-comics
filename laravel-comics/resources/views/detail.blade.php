bb6b26ad9a2a8b780d4e50d5e3120e19d49c5081
Robin9800 <robin.costanzo@gmail.com>
2022-05-19 16:19:56 +0200

    $comic['series'] added

--- laravel-comics/resources/views/detail.blade.php
+++ laravel-comics/resources/views/detail.blade.php
@@ -2,10 +2,11 @@
 
 @section('content')
     <div class="detail/">
-        <h2>{{$comic['title']}}</h2>
+        <h2>{{$comic['series']}}</h2>
+        <h3>{{$comic['title']}}</h3>
         <img src="{{$comic['thumb']}}" alt="">
         <p>{{$comic['description']}}</p>
-        <h3>{{$comic['price']}}</h3>
+        <h4>{{$comic['price']}}</h4>
     </div>
 
     <button>
